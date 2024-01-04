542d724eca5797b42681a29e903e5b48da1cb410
Sebastian Green <sebastianpetergreen@gmail.com>
2024-01-04 15:51:03 +0100

    cleanup

--- index.mjs
+++ index.mjs
@@ -2,12 +2,12 @@ import { readFileSync, writeFileSync } from "fs";
 import ical, { ICalEventBusyStatus, ICalEventRepeatingFreq } from "ical-generator";
 
 // Aggregated output from facebook graphql BirthdayCometMonthlyBirthdaysRefetchQuery
-const file = readFileSync("tmp/data.json", "utf-8");
+const facebookOutput = readFileSync("tmp/data.json", "utf-8");
 
-const parsed = JSON.parse(file);
+const parsedFacebookOutput = JSON.parse(facebookOutput);
 
 var users = [];
-extractByKey(users, parsed, "__typename", "User");
+deepExtractByKeyValue(users, parsedFacebookOutput, "__typename", "User");
 
 var birthdates = new Map();
 
@@ -15,22 +15,6 @@ for(const user of users){
     if (!!user.birthdate){
         birthdates.set(user.id, { name: user.name, birthdate: user.birthdate, url: user.profile_url });
     }
-}
-
-function extractByKey(out, obj, searchKey, searchVal) {
-  for (const key in obj) {
-    const val = obj[key];
-    if (key.match(searchKey) && (val?.match(searchVal) || false)) {
-      out.push(obj);
-    } else if (typeof(val) === 'object' && typeof(val?.length) === 'number'){
-        for(const iter of val){
-            extractByKey(out, iter, searchKey, searchVal)
-        }
-    }
-    else if (typeof(val) === 'object') {
-        extractByKey(out, val, searchKey, searchVal)
-    }
-  }
 }
 
 console.log('Birthdates caught: ', birthdates.length);
@@ -61,3 +45,19 @@ const events = Array.from(birthdates.entries()).map(([userId, bd]) => {
 calendar.events(events);
 
 writeFileSync("tmp/out.ical", calendar.toString());
+
+function deepExtractByKeyValue(out, obj, searchKey, searchVal) {
+    for (const key in obj) {
+      const val = obj[key];
+      if (key.match(searchKey) && (val?.match(searchVal) || false)) {
+        out.push(obj);
+      } else if (typeof(val) === 'object' && typeof(val?.length) === 'number'){
+          for(const iter of val){
+              deepExtractByKeyValue(out, iter, searchKey, searchVal)
+          }
+      }
+      else if (typeof(val) === 'object') {
+          deepExtractByKeyValue(out, val, searchKey, searchVal)
+      }
+    }
+  }
